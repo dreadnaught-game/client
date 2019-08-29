d9b67b9bfeba444f903925204efd615c2af05032
Dean Sumitro <DSumitro@talentpath.com>
2019-08-29 08:59:41 -0700

    Working on keybindings

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
 // alert('hello');
 const gameBoard = document.getElementById('gameBoard');
+// have a default start location (1,1)
+// eventlistener for keypress
+// keep track of current row/col
+// classList.remove(string)
+// classList.add(string)
+// querySelector
 
-for (let i = 1; i < 11; i++) {
-  for (let j = 1; j < 11; j++) {
-    const coordinate = `<div class="coordinate" data-row="${i}" data-col="${j}"></div>`;
-    gameBoard.innerHTML += coordinate;
-  }
-}
+const highlightSquare = target => {
+  console.log(target);
+  target.classList.add('marked');
+  console.log(
+    `${target.getAttribute('data-row')}, ${target.getAttribute(
+      'data-col'
+    )} marked`
+  );
+};
+// impure as of now
+
+const moveUp = currentSquare => {
+  console.log('up');
+};
+const moveLeft = currentSquare => {
+  console.log('left');
+};
+const moveDown = currentSquare => {
+  // console.log('down');
+  // const newSquare = document.getElementsByClassName('marked');
+  // newSquare.setAttribute(
+  //   'data-row',
+  //   `${parseInt(currentSquare.getAttribute('data-row')) + 1}`
+  // );
+  // `${parseInt(currentSquare.getAttribute('data-row')) + 1}, ${currentSquare.getAttribute('data-col')}`.split(', ');
+  // currentSquare.classList.remove('marked');
+  // console.log(currentSquare);
+  // console.log(newSquare);
+  highlightSquare(currentSquare);
+};
+const moveRight = currentSquare => {
+  console.log('right');
+};
+const confirmShot = currentSquare => {
+  console.log('fire!');
+};
+
+// const navButtons = {
+//   up: false,
+//   down: false,
+//   left: false,
+//   right: false,
+// };
+
+// function searchFleet(fleet, coordinate) { }
 
 // TODO: JSON documentation
 // TODO: Populate board with ships at random non-overlapping locations
@@ -62,8 +107,10 @@ const deployShips = () => {
 };
 
 const markCoordinate = (hitShip, [row, col]) => {
-  const uiCoordinate = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
-  console.log(hitShip);
+  const uiCoordinate = document.querySelector(
+    `[data-row="${row}"][data-col="${col}"]`
+  );
+  uiCoordinate.setAttribute('data-clicked', 'true');
   if (hitShip) {
     // changed coordinate square to red
     uiCoordinate.classList.add('hit');
@@ -84,39 +131,99 @@ const spaceIsOccupied = (fleet, coordinate) => {
   });
   return isOccupied;
 };
-
-const hitShip = (gameState, [row, col]) => {
+// really impure
+const countSunkShips = fleet => {
+  let count = 0;
+  fleet.forEach(ship => {
+    if (Object.keys(ship).length === 0) count += 1;
+  });
+  return count;
+};
+const removeShipCoordinate = ({ fleet }, [row, col]) => {
   // Let's try to sink some ship here
-  console.log('inside hitShip');
+  fleet.forEach(ship => {
+    Object.keys(ship).forEach(key => {
+      if (ship[key][0] === row && ship[key][1] === col) {
+        delete ship[key];
+      }
+    });
+  });
 };
 
 const fireCannon = (gameState, [row, col]) => {
+  const uiCoordinate = document.querySelector(
+    `[data-row="${row}"][data-col="${col}"]`
+  );
+  if (uiCoordinate.getAttribute('data-clicked') === 'true') {
+    // TODO: implement a return message
+    return 0;
+  }
   markCoordinate(spaceIsOccupied(gameState.fleet, [row, col]), [row, col]);
-  hitShip(gameState, [row, col]);
-
+  removeShipCoordinate(gameState, [row, col]);
   // TODO: check if a space is occupied by a ship
   // TODO: keep track of the move
   // TODO: check if a ship is sunk
 };
 
+const reset = () => {
+  gameBoard.innerHTML = '';
+};
 const hoistSails = () => {
+  reset();
+  for (let i = 1; i < 11; i++) {
+    for (let j = 1; j < 11; j++) {
+      const coordinate = `<div class="coordinate" data-row="${i}" data-col="${j}"></div>`;
+      gameBoard.innerHTML += coordinate;
+    }
+  }
   const gameState = {
-    sunkShips: 0,
     fleet: deployShips(),
+    currentSquare: document.querySelector(`[data-row="1"][data-col="1"]`),
   };
+  // TODO: move into a createBoard function
   // TODO: listen for user input
+
   const listOfCoords = document.querySelectorAll('.coordinate');
+  // console.log(listOfCoords);
   listOfCoords.forEach(coordinate => {
     coordinate.addEventListener('click', () => {
-      fireCannon(gameState, [parseInt(coordinate.dataset.row), parseInt(coordinate.dataset.col)]);
+      fireCannon(gameState, [
+        parseInt(coordinate.dataset.row),
+        parseInt(coordinate.dataset.col),
+      ]);
+      if (countSunkShips(gameState.fleet) === 5) {
+        // eslint-disable-next-line no-restricted-globals
+        const response = confirm(
+          "Shiver me timbers! Ye sunk all thar ships and seized th' treasure, Captain! But thar be more booty to be had, Play Again?"
+        );
+        if (response) hoistSails();
+        else return 0;
+      }
     });
   });
+  console.log(gameState.currentSquare);
+  document.body.onkeydown = function(event) {
+    switch (event.keyCode) {
+      case 87:
+        moveUp(hoistSails.currentSquare);
+        break;
+      case 65:
+        moveLeft(hoistSails.currentSquare);
+        break;
+      case 68:
+        moveRight(hoistSails.currentSquare);
+        break;
+      case 83:
+        console.log(gameState.currentSquare);
+        moveDown(gameState.currentSquare);
+        break;
+      case 13:
+        highlightSquare(hoistSails.currentSquare);
+        break;
+      default:
+        break;
+    }
+  };
 };
-
-// TODO: check if all ships are sunk (Win Condition)
-// TODO: gamestate tracker (OBJ/ARRAY)
-// Recursively call from register move -> Win Condition
-
-// TODO: Play Again --> populate board
 
 hoistSails();
